07d71ae4eadc9a194a121804fdf2b4a71deebeed
Andrew Obiano <andrew.obiano@gmail.com>
2019-07-13 10:41:45 -1000

    Finished

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -3,7 +3,7 @@ exports.largestPrimeFactor = function(n) {
   // do your work here
   let tempArr = [];
 
-  // prime number generator
+  // prime number checker
   function isPrime(num) {
     if (num <= 1) {
       // ignore 0 and 1
@@ -25,10 +25,11 @@ exports.largestPrimeFactor = function(n) {
       i += 6;
     }
     return true;
-  } // end of prime number generator
+  } // end of prime number checker
 
   for (let i = 0; i < 7000; i++) {
-    // put primes in an array
+    // put prime numbers only in the temp array
+    // only did up to 7000 for sake of challenge
     if (isPrime(i) === true) {
       tempArr.push(i);
     }
